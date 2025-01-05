f78a9cbb1bc85ee481cdad1c286cfc369a213506
aminsoraya <aminsoraya@gmail.com>
2025-01-05 14:43:44 +0330

    adding loading to project

--- components/trendyol/batchLinks.tsx
+++ components/trendyol/batchLinks.tsx
@@ -3,6 +3,7 @@ import { BatchLink } from "@/types/trendyol";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Loading from "../common/Loading";
 
 interface IProps {
   data: BatchLink[];
@@ -10,19 +11,28 @@ interface IProps {
 
 export default function BatchLinks({ data }: IProps) {
   return (
-    <div className="w-full grid grid-cols-2 md:grid-cols-3 gap-4 my-3">
-      {data.map((item, index) => (
-        <Link key={index} href={item.link} className="block aspect-video relative">
-          <Image
-            src={item.img}
-            alt=""
-            fill
-            sizes="(max-width: 668px) 70vw, 33vw"
-            className="object-cover rounded-lg"
-            priority={index < 6}
-          />
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className="w-full grid grid-cols-2 md:grid-cols-3 gap-4 my-3">
+        {data.map((item, index) => (
+          <Link
+            key={index}
+            href={item.link}
+            className="block aspect-video relative"
+          >
+            <Image
+              src={item.img}
+              alt=""
+              fill
+              sizes="(max-width: 668px) 70vw, 33vw"
+              className="object-cover rounded-lg"
+              priority={index < 6}
+            />
+          </Link>
+        ))}
+      </div>
+      <div className="w-full flex justify-center h-12 my-3">
+        <Loading />
+      </div>
+    </>
   );
 }
