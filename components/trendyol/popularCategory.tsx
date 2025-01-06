3f2380cb0bfdf452e9be61db2940906f5f199746
aminsoraya <aminsoraya@gmail.com>
2025-01-06 11:11:52 +0330

    add title and set links to popular categories

--- components/trendyol/popularCategory.tsx
+++ components/trendyol/popularCategory.tsx
@@ -1,25 +1,30 @@
+"use client"
 import { TrendyolMain } from "@/types";
 import React from "react";
 import Image from "next/image";
 import { PopularCategory } from "@/types/trendyol";
+import Link from "next/link";
 
 interface IProps {
   data: PopularCategory[];
 }
 export default function PopularCategoryComponent({ data }: IProps) {
   return (
-    <div className="w-full   gap-3 flex flex-wrap justify-between">
-      {data.map((item, index) => (
-        <div className="relative overflow-hidden rounded " key={index}>
-          <Image
-            className="bg-cover"
-            alt=""
-            src={item.img}
-            width={150}
-            height={150}
-          />
-        </div>
-      ))}
+    <div className="w-full flex flex-col">
+      <span className="py-5">دسته بندی های محبوب </span>
+      <div className="w-full   gap-3 flex flex-wrap justify-between">
+        {data.map((item, index) => (
+          <Link href={item.link} className="relative overflow-hidden rounded " key={index}>
+            <Image
+              className="bg-cover"
+              alt=""
+              src={item.img}
+              width={150}
+              height={150}
+            />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
