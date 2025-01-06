5863f99452db39698e6742fc6892fa656818caa0
aminsoraya <aminsoraya@gmail.com>
2025-01-06 09:46:28 +0330

    fix space button of browser(main issue is brands component)

--- app/trendyol/page.tsx
+++ app/trendyol/page.tsx
@@ -39,7 +39,6 @@ export default async function TrendyolComponent() {
         <UsefulProductComponent data={fetchedData.usefulProducts} />
       )}
       {fetchedData.batchLinks && <BatchLinks data={fetchedData.batchLinks} />}
-      
     </Container>
   );
 }
