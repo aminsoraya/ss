5073878f669e101ffa20aec0042c09d58cfc87c1
aminsoraya <aminsoraya@gmail.com>
2025-01-07 14:33:23 +0330

    remove useClient from batchLinks component

--- components/trendyol/batchLinks.tsx
+++ components/trendyol/batchLinks.tsx
@@ -1,33 +1,19 @@
-"use client";
 import { BatchLink } from "@/types/trendyol";
-import React, { useEffect, useState } from "react";
-import Loading from "../common/Loading";
-import { useInView } from "react-intersection-observer";
+import React from "react";
 import BatchLinksDetail from "./batchLinksDetail";
+import BatchLinksClient from "./batchLinksClient";
 
 interface IProps {
   data: BatchLink;
 }
 
 export default function BatchLinks({ data }: IProps) {
-  const { ref: inViewRef, inView } = useInView();
-  const [page, setPage] = useState(0);
   const { currentPage, totalCount, ...remain } = data;
-
-  useEffect(() => {
-    setPage((state) => state + 1);
-  }, [inView]);
-
-  useEffect(() => {
-    console.log(page);
-  }, [page]);
 
   return (
     <>
       <BatchLinksDetail list={remain} />
-      <div className="w-full flex justify-center h-12 my-3" ref={inViewRef}>
-        <Loading />
-      </div>
+      <BatchLinksClient />
     </>
   );
 }
