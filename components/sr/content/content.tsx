a631e2a8043e2209df5a35162d46c31926e89153
aminsoraya <aminsoraya@gmail.com>
2025-01-11 13:29:32 +0330

    seperate header of main content

--- components/sr/content/content.tsx
+++ components/sr/content/content.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Content() {
-  return (
-    <div className='  w-full px-3 py-3'>content</div>
-  )
+  return <div className="  w-full px-3 py-1">
+    <Header/>
+  </div>;
 }
 
- 
+const Header = () => {
+  return (
+    <div className="h-12 bg-gray-200 flex items-center justify-between ">
+      <span>تعداد نتایج</span>
+      <span>مرتب سازی</span>
+    </div>
+  );
+};
