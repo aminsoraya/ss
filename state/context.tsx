130a4046e7c8d7bad2c8e648d83273ea5eb6415b
aminsoraya <aminsoraya@gmail.com>
2025-01-12 15:20:41 +0330

    update context for showing filters

--- state/context.tsx
+++ state/context.tsx
@@ -1,20 +1,29 @@
-"use client"
+"use client";
 import { createContext, ReactNode, useContext, useState } from "react";
 
 interface ContextType {
   showSort: boolean;
+  showFilters: boolean;
+  setShowFilters: React.Dispatch<React.SetStateAction<boolean>>;
   setShowSort: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const TrendyolContext = createContext<ContextType | undefined>(undefined);
 
-export const TrendyolContextProvider = ({ children }: { children: ReactNode }) => {
+export const TrendyolContextProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
   const [showSort, setShowSort] = useState<boolean>(false);
+  const [showFilters, setShowFilters] = useState<boolean>(false);
   return (
-    <TrendyolContext.Provider value={{ showSort: showSort, setShowSort }}>
+    <TrendyolContext.Provider
+      value={{ showSort, setShowSort, showFilters, setShowFilters }}
+    >
       {children}
     </TrendyolContext.Provider>
   );
 };
 
-export const useTrendyolContext=()=>useContext(TrendyolContext)!
+export const useTrendyolContext = () => useContext(TrendyolContext)!;
