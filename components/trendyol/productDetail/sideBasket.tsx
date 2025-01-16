f4d465f1501714223f60b93fe48a4d45a30d2959
aminsoraya <aminsoraya@gmail.com>
2025-01-16 11:27:42 +0330

    add protection plan to the sidebasket and reorder button components with protection plan (repositioned)

--- components/trendyol/productDetail/sideBasket.tsx
+++ components/trendyol/productDetail/sideBasket.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { TrendyolProductDetail } from "@/types/trendyol";
 import React, { useState } from "react";
 import { Price } from "./specification";
@@ -6,7 +6,6 @@ import {
   Command,
   CommandEmpty,
   CommandGroup,
-  CommandInput,
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
@@ -15,6 +14,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { ChevronsUpDown } from "lucide-react";
 
 export default function SideBasket(
   props: Pick<
@@ -29,30 +29,57 @@ export default function SideBasket(
         originalPrice={props.originalPrice}
       />
       <InStock remainCount={props.remainCount!} />
+      <ProtectionPlan />
+      <ShoppingButtons />
     </div>
   );
 }
 
 const InStock = (props: Pick<TrendyolProductDetail, "remainCount">) => {
-  const [open, setOpen] = useState<boolean | undefined>();
   return (
-    <div>
-      <span>در انبار</span>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverContent className="w-[200px] p-0">
-          <Command>
-            <CommandInput placeholder="Search framework..." />
-            <CommandList>
-              <CommandEmpty>No framework found.</CommandEmpty>
-              <CommandGroup>
-                {Array.from({ length: props.remainCount }).map((_, index) => (
-                  <CommandItem key={index}>{index + 1}</CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
+    <div className="my-5">
+      <div className="my-2">
+        <span className="text-green-700 py-5">در انبار</span>
+      </div>
+      <select className="w-full h-12 px-3 rounded bg-gray-50 border">
+        {Array.from({ length: props.remainCount }).map((_, index) => (
+          <option value={index + 1}>{index + 1}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+const ShoppingButtons = () => {
+  return (
+    <div className="flex items-center gap-y-3 my-5 flex-col">
+      <button className="bg-orange-500 h-12 w-full px-5 text-white rounded hover:bg-orange-700">
+        خرید محصول
+      </button>
+
+      <button className="border-orange-500 h-12 w-full px-5 text-orange-500 hover:bg-orange-500 hover:text-white border rounded">
+        افزودن به سبد خرید
+      </button>
+    </div>
+  );
+};
+
+const ProtectionPlan = () => {
+  return (
+    <div className="flex items-center gap-y-3 my-5 flex-col justify-start w-full ">
+      <span className="w-full">انتخاب گارانتی :</span>
+      <div className="w-full">
+        <input type="checkbox" name="" id="" />
+        <span className="px-1 text-xs text-gray-700">
+          گارانتی یک ساله موجوجم{" "}
+        </span>
+      </div>
+      <div className="w-full">
+        <input type="checkbox" name="" id="" />
+        <span className="px-1 text-xs text-gray-700">
+          گارانتی دو ساله موجوجم{" "}
+        </span>
+      </div>
     </div>
   );
 };
